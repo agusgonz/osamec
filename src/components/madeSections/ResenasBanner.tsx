a6a0fc904ea065a641d4a2c446eff24d52ededca
agusgonz <agustingendeiza@gmail.com>
2023-11-17 11:51:04 -0300

    Fix image opacity

--- src/components/madeSections/ResenasBanner.tsx
+++ src/components/madeSections/ResenasBanner.tsx
@@ -6,9 +6,11 @@ interface ReseñasBannerProps {}
 const ReseñasBanner: FC<ReseñasBannerProps> = ({}) => {
 	return (
 		<div className="relative">
-			<div className="w-full h-full absolute bg-[url('/images/image-6.jpg')] bg-fixed bg-cover -z-10 brightness-50 "></div>
+			<div className="w-full h-full absolute bg-[url('/images/image-6.jpg')] bg-fixed bg-cover -z-10 ">
+				<div className="w-full h-full bg-[rgba(0,0,0,0.7)] "></div>
+			</div>
 
-			<div className=" flex flex-col text-center items-center  px-6 gap-6 italic  text-white  py-12">
+			<div className="container mx-auto flex flex-col text-center items-center  px-6 gap-6 italic  text-white  py-12">
 				<PencilLine className="w-12 h-12  " />
 
 				<p>
